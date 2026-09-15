8565f90bbd5e5b49334e0b57f694b9a46a9817cf
x!n.g <>
2026-09-15 17:48:33 +0800

    fix: import PrismaModule in InitModule and AuthModule
    PrismaService was not available in these modules causing DI error on startup.

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
--- server/src/modules/init/init.module.ts
+++ server/src/modules/init/init.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InitService } from './init.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [InitService],
 })
 export class InitModule {}
